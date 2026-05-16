4d4a5a8f24344a63f36041cc43161c5a18eabf66
donclaudito <clauorenstein@gmail.com>
2026-05-16 10:53:23 -0300

    chore: add future flags to BrowserRouter to silence v7 deprecation warnings

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
+        <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <Routes>
             {/* Rotas Públicas */}
             <Route path="/login" element={<Login />} />
